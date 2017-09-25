8a7de7c8ff0e5ab6da565d1956cfd2a1869d774d
wesam-taha <tahawesam@yahoo.com>
2017-09-25 20:49:54 +0400

    www
    www

--- www/js/init-app.js
+++ www/js/init-app.js
@@ -1,3 +1,25 @@
+
+function myEventHandler() {
+    "use strict" ;
+
+    var ua = navigator.userAgent ;
+    var str ;
+
+    if( window.Cordova && dev.isDeviceReady.c_cordova_ready__ ) {
+            str = "It worked! Cordova device ready detected at " + dev.isDeviceReady.c_cordova_ready__ + " milliseconds!" ;
+    }
+    else if( window.intel && intel.xdk && dev.isDeviceReady.d_xdk_ready______ ) {
+            str = "It worked! Intel XDK device ready detected at " + dev.isDeviceReady.d_xdk_ready______ + " milliseconds!" ;
+    }
+    else {
+        str = "Bad device ready, or none available because we're running in a browser." ;
+    }
+
+    alert(str) ;
+}
+
+
+
 /*
  * Please see the included README.md file for license terms and conditions.
  */
